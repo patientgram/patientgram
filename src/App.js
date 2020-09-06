1d0cb895d0bddee9f75fa5e710b7634151d50afc
emilyham <emilyham98@gmail.com>
2020-09-05 22:42:22 -0400

    home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {Route, Switch, Redirect} from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
+import homepagePicture from "./images/homepage-picture.jpg";
 
 import Placeholder from "./pages/placeholder/placeholder";
 import ProviderDashboard from "./pages/provider-dashboard/provider-dashboard";
@@ -8,29 +9,71 @@ import ContactDashboard from "./pages/contact-dashboard/contact-dashboard";
 import SignUp from "./pages/sign-up/sign-up";
 import SignIn from "./pages/sign-in/sign-in";
 import Page404 from "./pages/page-404/page-404";
+import Header from "./header";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      currentUser: null
+      currentUser: null,
     };
   }
 
   render() {
-    return (
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={Placeholder} />
-          <Route exact path="/providerdashboard" component={ProviderDashboard} />
-          <Route exact path="/providerdashboard/:patientId" component={ProviderDashboard} />
-          <Route exact path="/contactdashboard" component={ContactDashboard} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/signin" component={SignIn} />
-          <Route component={Page404} />
-        </Switch>
-      </div>
-    );
+    if (this.state.currentUser) {
+      return (
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Placeholder} />
+            <Route
+              exact
+              path="/providerdashboard"
+              component={ProviderDashboard}
+            />
+            <Route
+              exact
+              path="/providerdashboard/:patientId"
+              component={ProviderDashboard}
+            />
+            <Route
+              exact
+              path="/contactdashboard"
+              component={ContactDashboard}
+            />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/signin" component={SignIn} />
+            <Route component={Page404} />
+          </Switch>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Header
+            title="PatientGram"
+            sections={[{ title: "Technology", url: "#" }]}
+          />
+          <div>
+            <img className="image" src={homepagePicture}></img>
+          </div>
+          <div className="description">
+            <p>
+              At PatientGram, we believe that keeping your loved one's updated
+              is an essential part of patient care. This platform allows you to
+              receive direct updates from doctors when there are changes to
+              their care plan. Simply create an account and get updates about
+              your loved one's care progress
+            </p>
+            <h3>How it works</h3>
+            <p>
+              A medical provider working with the patient will provide frequent
+              updates. As the contact person for the patient, you will get a
+              notification when there is a new update.
+            </p>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
---
+++ src/header.js
@@ -0,0 +1,68 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import logo from "./images/logo.png";
+
+const useStyles = makeStyles((theme) => ({
+  image: {
+    height: theme.spacing(6),
+  },
+  toolbar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbarTitle: {
+    flex: 1,
+  },
+  toolbarSecondary: {
+    justifyContent: "space-between",
+    overflowX: "auto",
+  },
+  toolbarLink: {
+    padding: theme.spacing(1),
+    flexShrink: 0,
+  },
+}));
+
+export default function Header(props) {
+  const classes = useStyles();
+  const { title } = props;
+
+  return (
+    <React.Fragment>
+      <Toolbar className={classes.toolbar}>
+        <img className={classes.image} src={logo}></img>
+        <Typography
+          component="h2"
+          variant="h5"
+          color="inherit"
+          align="center"
+          noWrap
+          className={classes.toolbarTitle}
+        >
+          {title}
+        </Typography>
+        <Button variant="outlined" size="small">
+          <Link
+            color="inherit"
+            noWrap
+            key={"login"}
+            variant="body2"
+            href={"/login"}
+            className={classes.toolbarLink}
+          >
+            {"Login"}
+          </Link>
+        </Button>
+      </Toolbar>
+    </React.Fragment>
+  );
+}
+
+Header.propTypes = {
+  sections: PropTypes.array,
+  title: PropTypes.string,
+};
